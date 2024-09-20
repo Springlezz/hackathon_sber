d573b1875043e75adcfddf00eba501b474e3db09
Zuenko <zuenko.michail@gmail.com>
2024-09-20 20:34:59 +0700

    deleteEvent API

---
+++ src/api/deleteEvent.js
@@ -0,0 +1,15 @@
+import checkProps from '../checkProps.js';
+import { dbGet, dbRun } from '../db.js';
+
+export async function post(db, { userId, body }) {
+    if (userId === null) return [401, { error: 'Пользователь не авторизован.' }];
+    const [{ role }] = await dbGet(db, 'SELECT role FROM users WHERE id = ?', userId);
+    if (role === 0) return [401, { error: 'Недостаточно прав для совершения действия.' }];
+    const check = checkProps(['id'], body);
+    if (check) return [400, { error: check }];
+    if (role === 1 && (await dbGet(db, 'SELECT creator FROM events WHERE id = ?', body.id))[0].creator !== userId) return [401, { error: 'Недостаточно прав для совершения действия.' }];
+
+    await dbRun(db, 'DELETE FROM events WHERE id = ?', body.id);
+
+    return [200, {}];
+}
