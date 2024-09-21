fb2b2eda0c5bbf0821060e3814b5b0ae94d873ba
Zuenko <zuenko.michail@gmail.com>
2024-09-21 11:15:28 +0700

    Add country in getUserInfo

--- src/api/getUserInfo.js
+++ src/api/getUserInfo.js
@@ -3,13 +3,14 @@ import { dbGet } from '../db.js';
 export async function get(db, { userId }) {
     if (userId === null) return [401, { error: 'Пользователь не авторизован.' }];
 
-    const [user] = await dbGet(db, 'SELECT email, role, city, first_name, second_name, third_name FROM users WHERE id = ?', userId);
+    const [user] = await dbGet(db, 'SELECT email, role, first_name, second_name, third_name, country, city FROM users WHERE id = ?', userId);
     return [200, {
         email: user.email,
         role: user.role,
-        city: user.city,
         firstName: user.first_name,
         secondName: user.second_name,
-        thirdName: user.third_name
+        thirdName: user.third_name,
+        country: user.country,
+        city: user.city
     }];
 }
