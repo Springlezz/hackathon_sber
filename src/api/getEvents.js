690841cfdac8507c7eee73ef747d8a0ba54afe1a
Zuenko <zuenko.michail@gmail.com>
2024-09-21 08:38:33 +0700

    Fix getEvent API with tags

--- src/api/getEvents.js
+++ src/api/getEvents.js
@@ -11,6 +11,7 @@ export async function get(db, { userId, search }) {
         JOIN event_tags ON event_tags.event_id = events.id
         WHERE events.time >= ? AND events.time <= ?
     `;
+
     let eventTags = [];
     if (search.tags) {
         eventTags = search.tags.split(',');
@@ -19,19 +20,18 @@ export async function get(db, { userId, search }) {
         }
         sql += `AND event_tags.tag_id IN (${eventTags.map(() => '?').join(', ')})`;
     }
-    else {
-        const [[events], role] = await Promise.all([
-            dbAll(db, sql, search.timeStart, search.timeEnd, ...eventTags),
-            userId === null ? 0 : dbGet(db, 'SELECT role FROM users WHERE id = ?', userId).then(user => user[0].role)
-        ]);
-        return [200, {
-            events: events.filter(event => role === 2 || event.creator === userId || (event.confirmed && event.accepted)).map(event => ({
-                title: event.title,
-                description: event.description,
-                time: event.time,
-                duration: event.duration,
-                location: event.location
-            }))
-        }];
-    }
+
+    const [[events], role] = await Promise.all([
+        dbAll(db, sql, search.timeStart, search.timeEnd, ...eventTags),
+        userId === null ? 0 : dbGet(db, 'SELECT role FROM users WHERE id = ?', userId).then(user => user[0].role)
+    ]);
+    return [200, {
+        events: events.filter(event => role === 2 || event.creator === userId || (event.confirmed && event.accepted)).map(event => ({
+            title: event.title,
+            description: event.description,
+            time: event.time,
+            duration: event.duration,
+            location: event.location
+        }))
+    }];
 }
