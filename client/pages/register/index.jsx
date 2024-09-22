5ace1486715122af4970925bb795f871a54fda1c
Zuenko <zuenko.michail@gmail.com>
2024-09-22 09:23:03 +0700

    Стили

--- client/pages/register/index.jsx
+++ client/pages/register/index.jsx
@@ -1,33 +1,34 @@
 import btnStyles from '../../components/button.scss';
 import formStyles from '../../components/form.scss';
+import inpStyles from '../../components/input.scss';
 import Link from '../../components/link.jsx';
 import { postApi } from '../../lib/api.js';
 
 export default function Register({ setTitle, mainAppend, authorized, goPage }) {
     setTitle('Регистрация');
 
-    const email = <input type="email" required />;
-    const password = <input type="password" required />;
-    const password2 = <input type="password" required />;
+    const email = <input class={inpStyles.input} type="email" required />;
+    const password = <input class={inpStyles.input} type="password" required />;
+    const password2 = <input class={inpStyles.input} type="password" required />;
 
-    const firstName = <input type="text" required />;
-    const secondName = <input type="text" required />;
-    const thirdName = <input type="text" />;
+    const firstName = <input class={inpStyles.input} type="text" required />;
+    const secondName = <input class={inpStyles.input} type="text" required />;
+    const thirdName = <input class={inpStyles.input} type="text" />;
 
-    const city = <input type="text" required />;
-    const country = <input type="text" required />;
+    const city = <input class={inpStyles.input} type="text" required />;
+    const country = <input class={inpStyles.input} type="text" required />;
 
     const $error = <></>;
     const form = (
         <form class={formStyles.form}>
-            <label>Почта: {email}</label>
-            <label>Пароль: {password}</label>
-            <label class={formStyles.gap}>Повторите пароль: {password2}</label>
-            <label>Имя: {firstName}</label>
-            <label>Фамилия: {secondName}</label>
-            <label class={formStyles.gap}>Отчество: {thirdName}</label>
-            <label>Город: {city}</label>
-            <label>Страна: {country}</label>
+            <label class={formStyles.label}>Почта: {email}</label>
+            <label class={formStyles.label}>Пароль: {password}</label>
+            <label classes={[formStyles.label, formStyles.gap]}>Повторите пароль: {password2}</label>
+            <label class={formStyles.label}>Имя: {firstName}</label>
+            <label class={formStyles.label}>Фамилия: {secondName}</label>
+            <label classes={[formStyles.label, formStyles.gap]}>Отчество: {thirdName}</label>
+            <label class={formStyles.label}>Город: {city}</label>
+            <label class={formStyles.label}>Страна: {country}</label>
             <div class={formStyles.error}>{$error}</div>
             <input type="submit" value="Зарегистрироваться" class={btnStyles.button} onClick={register} />
             <Link class={btnStyles.button} href="/register/telegram/" onClick={goPage}>Зарегистрироваться через Telegram</Link>
